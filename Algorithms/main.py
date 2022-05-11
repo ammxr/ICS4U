8d720f43c42c4fd22ddee06b825adfcd62477102
ammxr <ammarhakim57@gmail.com>
2022-05-11 18:51:19 -0400

    Search Setup
    Linear + Binary setup/fix

--- Algorithms/main.py
+++ Algorithms/main.py
@@ -3,12 +3,17 @@ import time
 import random
 import copy
 from insertionSort import insertionSort
+from binarySearch import binarySearch
+from linearSearch import linearSearch
 
+def Average(arr):
+    return sum(arr) / len(arr)
+  
 # Process Time
-startTime = time.time()
 def getTime(start):
-  endTime= (time.time()) - startTime
+  endTime= (time.time()) - start
   print("    Took " + str(endTime) + " seconds to run")
+  return endTime
 
 # Random List Setup
 def getRandomList(listSize):
@@ -17,10 +22,6 @@ def getRandomList(listSize):
     randomList[j] = round(random.random() * listSize)
   return randomList
 
-# Data Array Generation
-dataSize=20
-randomList = getRandomList(dataSize)
-print(str(randomList))
 # Sorting
 def sortArr(arr, type):
   if type.lower()=="default":
@@ -30,13 +31,37 @@ def sortArr(arr, type):
   else:
     return("Invalid Selection")
 
-#  Main Program
-type=input("What sort would you like to run? ")
-sortArr(randomList, type)
+def search(arr, type, value):
+  if type.lower()=="linear":
+    return linearSearch(arr, value)
+  elif type.lower()=="binary":
+    return binarySearch(arr, value)
+  else:
+    return("Invalid Selection")
+    
+# Main Program
+    
+#   Data Array Generation
+dataSize=20
+randomList = getRandomList(dataSize)
+print(str(randomList))
 
-# Number of Trials
+#   Trials and Timings
+type=input("What sort would you like to run? ")
 numTrials= 10
+timesList=[]
+
 for i in range(numTrials):
   print("Loop #" +str(i+1)+" out of " + str(numTrials))
   randomList = getRandomList(dataSize)
-  timer = getTime()
+  startTime = time.time()
+  sortArr(randomList, type)
+  timer = getTime(startTime)
+  timesList.append(timer)
+print('\n')
+print("The average time was "+str(Average(timesList)))
+print(randomList)
+
+searchingType= input("What type of searching will you conduct? ")
+searchValue = int(input("What value are you searching? "))
+search(randomList, searchingType, searchValue)
