3fe2ea01a8b8afae42c363a6ad2ada86991e9ad1
ammxr <ammarhakim57@gmail.com>
2022-05-10 22:08:22 -0400

    Update main.py

--- Algorithms/main.py
+++ Algorithms/main.py
@@ -1,3 +1,4 @@
+# function in function
 import time
 import random
 import copy
@@ -5,9 +6,9 @@ from insertionSort import insertionSort
 
 # Process Time
 startTime = time.time()
-def getTime():
+def getTime(start):
   endTime= (time.time()) - startTime
-  print("The process took " + str(endTime) + " seconds to run")
+  print("    Took " + str(endTime) + " seconds to run")
 
 # Random List Setup
 def getRandomList(listSize):
@@ -17,18 +18,25 @@ def getRandomList(listSize):
   return randomList
 
 # Data Array Generation
-
 dataSize=20
 randomList = getRandomList(dataSize)
 print(str(randomList))
+# Sorting
+def sortArr(arr, type):
+  if type.lower()=="default":
+    return arr.sort()
+  elif type.lower()=="insertion":
+    return insertionSort(arr)
+  else:
+    return("Invalid Selection")
 
 #  Main Program
+type=input("What sort would you like to run? ")
+sortArr(randomList, type)
 
 # Number of Trials
 numTrials= 10
 for i in range(numTrials):
   print("Loop #" +str(i+1)+" out of " + str(numTrials))
   randomList = getRandomList(dataSize)
-  t = getTime()
-  t2 = getTime(randomList, "defaultSort")
-  timesDefault[i] = t2
+  timer = getTime()
