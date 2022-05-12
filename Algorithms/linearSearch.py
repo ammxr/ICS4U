f994b57fe78f8da150384bec1a6df91f82358912
ammxr <ammarhakim57@gmail.com>
2022-05-12 17:17:17 -0400

    Object Oriented Linear Search

--- Algorithms/linearSearch.py
+++ Algorithms/linearSearch.py
@@ -1,5 +1,5 @@
 def linearSearch(arr,value):
   for i in range(len(arr)):
-    if arr[i]== value:
-      return i
-  return -1
+    if arr[i].x== value:
+      print("Value found at index " + str(i))
+      break
